6e79b51a21008734c6014a1722d99d5d1a23f0d4
Aashi <165303579+Aktagh1234@users.noreply.github.com>
2025-03-09 06:02:57 +0000

    Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let error="ERROR";
+    let finalSum = 0;
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return error;
+    }
+    else if (typeof num1 === "object" || typeof num2 === "object") {
+        return error;
+    }
+    else if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return error;
+    }
+    else if (start>=0 && end>=0) {
+        for (let i = start; i <= end; i++) {
+            finalSum += i;
+        }
+    }
+    else {
+        return error;
+    }
+    return finalSum; 
 
 };
 
