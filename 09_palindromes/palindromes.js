8586a0ca709cb7aea79fda133782c6070b55c80a
Aashi <165303579+Aktagh1234@users.noreply.github.com>
2025-05-04 07:13:50 +0000

    Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+    const palindromes = function (str) {
+        // Remove non-alphanumeric characters and convert to lowercase
+        const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+        // Check if the cleaned string is equal to its reverse
+        return cleanedStr === cleanedStr.split('').reverse().join('');
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
